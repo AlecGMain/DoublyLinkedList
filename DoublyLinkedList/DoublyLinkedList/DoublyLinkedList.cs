8c50b0df0a45c4c55accc2dca5bcab9d2d74f24d
GMRDL1 <gmrstudent@outlook.com>
2018-05-26 14:29:23 -0700

    All functions complete. Now entering debugging stage.

--- DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -8,11 +8,11 @@ namespace DoublyLinkedList
 {
     class DoublyLinkedList<T> where T : IComparable<T>
     {
-        public Node<T> Head { get; set; }
-
-        public DoublyLinkedList() 
-            : this (null)
-        {}
+        public Node<T> Head;
+        public int Count;
+        public DoublyLinkedList()
+            : this(null)
+        { }
 
         public DoublyLinkedList(Node<T> head)
         {
@@ -23,6 +23,7 @@ namespace DoublyLinkedList
         {
             if (Head.Value == null)
             {
+                Head = new Node<T>();
                 Head.Value = value;
             }
             else
@@ -32,49 +33,75 @@ namespace DoublyLinkedList
                 {
                     current = current.Next;
                 }
+                current.Next = new Node<T>();
                 current.Next.Value = value;
-                current.Next.Previous = current.Next;
+                current.Next.Previous = current;
                 Head = current;
             }
+            Count++;
         }
         public void AddToFront(T value)
         {
-            if (Head.Value == null)
+            if (Head == null)
             {
+                Head = new Node<T>();
                 Head.Value = value;
             }
             else
             {
                 Node<T> tempNode = new Node<T>(value);
+                tempNode.Next = new Node<T>();
                 tempNode.Next = Head;
                 tempNode.Next.Previous = tempNode;
                 Head = tempNode;
             }
+            Count++;
         }
         public void AddAt(int position, T value)
         {
-            if(Head.Value == null)
+            if (Head == null)
             {
+                Head = new Node<T>();
                 Head.Value = value;
             }
             else
             {
+                if(Head == null)
+                {
+                    Head = new Node<T>();
+                    Head.Value = value;
+                }
                 Node<T> current = Head;
-                for(int i = 0; i < position; i++)
+                for (int i = 0; i < position; i++)
                 {
                     current = current.Next;
                 }
-                current.Next.Next = current.Next;
-                current.Next.Value = value;
-                current.Next.Next.Previous = current.Next;
+                if(current.Next == null)
+                {
+                    current.Next = new Node<T>();
+                }
+                if(current.Next.Next == null)
+                {
+                    current.Next.Value = value;
+                }
+                else
+                {
+                    Node<T> temp = current.Next;
+                    current.Next.Value = value;
+                    current.Next.Next = temp;
+                    current.Next.Next.Previous = current.Next;
+                }
+                Head = current;
             }
+            Count++;
         }
         public bool RemoveFromFront()
         {
             if (Head != null)
-            { 
+            {
                 Head = Head.Next;
                 Head.Previous = null;
+                Count--;
                 return true;
             }
             else
@@ -84,21 +111,71 @@ namespace DoublyLinkedList
         }
         public bool RemoveFromEnd()
         {
-            if(Head == null)
+            if (Head == null)
             {
                 return false;
             }
             else
             {
                 Node<T> current = Head;
-                while(current.Next != null)
+                while (current.Next.Next != null)
                 {
-
+                    current = current.Next;
+                }
+                current.Next = null;
+                Head = current;
+                Count--;
+                return true;
+            }
+        }
+        public bool RemoveAt(int position)
+        {
+            if (Head == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (position == 0)
+                {
+                    this.RemoveFromFront();
+                    Count--;
+                    return true;
+                }
+                else
+                {
+                    int count = 1;
+                    Node<T> tester = Head;
+                    while (tester.Next != null)
+                    {
+                        count++;
+                        tester = tester.Next;
+                    }
+                    if (count >= position)
+                    {
+                        Node<T> current = Head;
+                        for (int i = 0; i < position - 1; i++)
+                        {
+                            current = current.Next;
+                        }
+                        current.Next = current.Next.Next;
+                        current.Next.Previous = current;
+                        Head = current;
+                        Count--;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
             }
         }
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+
+
     }
-
-    
-
 }
--- DoublyLinkedList/DoublyLinkedList/Program.cs
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -10,6 +10,13 @@ namespace DoublyLinkedList
     {
         static void Main(string[] args)
         {
+            DoublyLinkedList<int> myList = new DoublyLinkedList<int>();
+
+            myList.AddToFront(5);
+            myList.AddToEnd(3);
+            myList.AddAt(1, 1);
+            myList.RemoveFromEnd();
+            myList.RemoveFromFront();
         }
     }
 }
